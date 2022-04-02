b9a3c7fb39f76bf9d1ea174eabc1bd39802b3e24
pareshpatel <pareshpatel75@gmail.com>
2022-04-02 18:45:31 +0100

    updated manual file

---
+++ PdfManualClass.java
@@ -0,0 +1,49 @@
+package ManualTesting;
+
+
+
+public class PdfManualClass {
+
+
+    public static void main (String[]args) {
+
+        System.out.println("What Is Software");
+        System.out.println("What Is Manual Testing ");
+        System.out.println("What Are The Risks ");
+        System.out.println("What Tre The Benefits ");
+        System.out.println("Who Does The Testing");
+        System.out.println("Qualification Of Tester ");
+        System.out.println("Challenges Of Tester");
+        System.out.println("Challenges of Manual Testing");
+        System.out.println("Principle of Software Testing");
+        System.out.println("Test Plan And Its Benefits");
+        System.out.println("Test Plan Template");
+        System.out.println("Test Scenario+Template");
+        System.out.println("Test Case+Template");
+        System.out.println("Testing Technique");
+        System.out.println("What Is A Bug? ");
+        System.out.println("Bug Lifecycle");
+        System.out.println("Bug Template");
+        System.out.println("Severity Vs Priority");
+        System.out.println("Defect Tracking Tool (JIRA)");
+        System.out.print("Testing Levels White Box Testing Black Box Testing");
+        System.out.print("Static Vs Dynamic Testing");
+        System.out.print("Exploratory Testing , Ad-Hoc Testing ");
+        System.out.print("Software Development Models ");
+        System.out.print("Waterfall,Iterative,V-model,Agile Model");
+        System.out.println("Defination Of Ready/Done");
+        System.out.println("What Is TDD? ");
+        System.out.println("What Is BDD? ");
+        System.out.println("What Is Gherkins?");
+        System.out.println("BDD Key words. Features,Background,Scenario,Given,When,Then,And,But");
+        System.out.println("Maintenance");
+        System.out.println("What Is A Change Request?");
+        System.out.println("What is A Hotfix? ");
+        System.out.print("What is Automation Testing ");
+        System.out.print("Advantage And Disadvantages of Automation");
+        System.out.println("What is JAVA? Where Is Java Used? ");
+
+
+    }
+
+}
